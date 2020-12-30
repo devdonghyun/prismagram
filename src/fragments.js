19abee656fa2b894ae93b424076f8c969606d2f1
devDongHyun <ehdgus8725@naver.com>
2020-12-31 00:19:11 +0900

    See Full Posts
     change fragments for seeing comments

--- src/fragments.js
+++ src/fragments.js
@@ -1,13 +1,8 @@
-export const USER_FRAGMENT = `
-    fragment UserParts on User {
+export const COMMENT_FRAGMENT = `
+    fragment CommentParts on Comment {
         id
-        username
-        email
-        firstName
-        lastName
-        bio
-        posts {
-            id
-            caption
-        }
-}`;
+        text
+        user {
+            username
+        }     
+    }`;
