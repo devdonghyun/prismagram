55d992460da98f75bbebbd5df9dee442e818d5e6
devDongHYun <ehdgus8725@naver.com>
2021-02-04 21:45:48 +0900

    roll back...

--- src/schema.js
+++ src/schema.js
@@ -5,7 +5,7 @@ import { loadFilesSync } from "@graphql-tools/load-files";
 
 
 const allTypes = loadFilesSync(path.join(__dirname, "api/**/*.graphql"));
-const allResolvers = loadFilesSync(path.join(__dirname, "api/**/*.ts"));
+const allResolvers = loadFilesSync(path.join(__dirname, "api/**/*.js"));
 
 const schema = makeExecutableSchema({
     typeDefs: mergeTypeDefs(allTypes),
